3435d35aaab413f8da1fc9a36561ddb27bc49d69
Jihoon <jihoonlee.in@gmail.com>
2012-10-15 11:37:36 +0900

    provide symlink

--- roswww_pack/scripts/pack.py
+++ roswww_pack/scripts/pack.py
@@ -29,6 +29,8 @@ def get_packages():
 def parse_args():
     parser = argparse.ArgumentParser(description='Crawls all www directory in ros package path')
     parser.add_argument('-d','--destination',type=str,required=True,nargs=1,help='/home/yourdir/www')
+    parser.add_argument('-l','--link',type=str,required=True,nargs=1,help='symbolic link or hard copy("link"/"copy")')
+
     args = parser.parse_args()
     return args
 
@@ -43,7 +45,16 @@ def copy_directories(packs,subdir,dest):
         run('cp','-r',path,destination)
     print "Done"
 
-
+# www only for now. We can copy other directories later.
+def link_directories(packs,subdir,dest):
+    print "Start Linking..."
+    for p in packs:
+        path = p['path'] + '/' + subdir
+        name = p['name']
+        destination = dest +'/'+name
+        print "\tLinking " + name
+        run('ln','-sf',path,destination)
+    print "Done"
 
 if __name__=='__main__':
     subdir = 'www'
@@ -54,6 +65,12 @@ if __name__=='__main__':
     try:         
         os.mkdir(args.destination[0])
     except OSError as e:
-        print str(args.destination[0]) + "is directory already exist. Please choose different directory name"
+        print str(args.destination[0]) + " directory already exists"
 
-    copy_directories(packages,'www',args.destination[0])
+    if args.link[0].lower() == "link":
+        link_directories(packages,'www',args.destination[0])
+    elif args.link[0].lower() == "copy":
+        copy_directories(packages,'www',args.destination[0])
+    else:
+        print "Arugument Error " + args.link[0] + ". should use -l (copy/link)"
+
