fc055cbf9024beb62b5816fdb932d22fb633041b
Jihoon <jihoonlee.in@gmail.com>
2012-10-15 15:07:04 +0900

    style changed. throttling messages

--- topseong_front/www/js/TopseungDashboard.js
+++ topseong_front/www/js/TopseungDashboard.js
@@ -26,22 +26,33 @@ function(declare,lang,domStyle,widgetbase,dom)
             onConnect : function() {
                 this.batterySub.subscribe(lang.hitch(this,this.batteryListener));
                 this.onoffDeviceSub.subscribe(lang.hitch(this,this.onoffDeviceListener));
+                //this.connectDiv.innerHTML = 'On';
+                this.connectDiv.style.backgroundColor ='#00FF00';
             },
 
             onClose : function() {
                 this.batterySub.unsubscribe();
                 this.onoffDeviceSub.unsubscribe();
+                //this.connectDiv.innerHTML = 'Off';
+                this.connectDiv.style.backgroundColor ='#FF0000';
+//                this.connectDiv.innerHTML = 'Disconnected';
             },
 
             createDiv : function() {
+                var div = this.createElement(this.domNode,'Connection : ','150px','80%');
+                this.connectDiv = div;
+                this.connectDiv.style.width='15%';
+                this.connectDiv.innerHTML = '&nbsp';
+                this.connectDiv.style.backgroundColor ='#FF0000';
                 
-                var div = this.createElement(this.domNode,'Battery : ');
+                var div = this.createElement(this.domNode,'Battery : ','120px','50%');
                 this.batteryDiv = div;
+                this.batteryDiv.innerHTML = '&nbsp';
 
-                div = this.createElement(this.domNode,'Mode : ');
+                div = this.createElement(this.domNode,'Mode : ','120px','50%');
                 this.modeDiv = div;
 
-                div = this.createElement(this.domNode,'Vel :');
+                div = this.createElement(this.domNode,'Vel :','100px','50%');
                 this.velDiv = div;
             },
 
@@ -49,14 +60,24 @@ function(declare,lang,domStyle,widgetbase,dom)
                 this.batterySub = new ros.Topic({
                     name : this.batteryTopic,
                     type : this.batteryType,
+                    throttle_rate : 500,
                 });
                 this.onoffDeviceSub = new ros.Topic({
                     name : this.onoffDeviceTopic,
                     type : this.onoffDeviceType,
+                    throttle_rate : 500,
                 });
             },
 
             batteryListener : function(msg) {
+                var level = parseFloat(msg.battery_percentage);
+                console.log(level);
+                /*
+                console.log(level);
+                level = level / 100;
+                level = level * 16646400;
+                level = level + 65280;
+                console.log(level.toString(16));*/
                 this.batteryDiv.innerHTML = msg.battery_percentage;
             },
 
@@ -74,21 +95,24 @@ function(declare,lang,domStyle,widgetbase,dom)
                     this.velDiv.innerHTML = ' Slow';
             },
 
-            createElement : function(parent,key)
+            createElement : function(parent,key,width,width2)
             {
                  var div = document.createElement('div');
                  
                  div.style.display='inline-block';
-                 div.style.width='120px';
+                 div.style.textAlign='center';
+                 div.style.width=width;
                  // key
                  var strong = document.createElement('strong');
                  strong.style.display='inherit';
+                 strong.style.width=width2;
                  strong.innerHTML = key;
 
                  div.appendChild(strong);
 
                  var childdiv = document.createElement('div');
                  childdiv.style.display='inherit';
+                 childdiv.style.textAlign='center';
                  div.appendChild(childdiv);
                  parent.appendChild(div);
 
